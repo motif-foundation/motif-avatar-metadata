13ada71a13828617e337499ca0df0677fe160389
Steve <johnw@motif.foundation>
2022-03-11 21:50:30 +0300

    latest

--- types/motif/MotifAvatar20210905.d.ts
+++ types/motif/MotifAvatar20210905.d.ts
@@ -1,23 +1,19 @@
 
 export interface MotifAvatar20210905 {
-  assetType: string;
-         
-  blockchain: string;
-  
-  name: string;;
-   
-  website: string; 
-   
-  socials: string;
-   
-  favorites: string;
-   
-  profileBackground: string;
-  
-  followings:   string;
-   
-  mimeType:  string;
-    
-  version: string;
+assetType: string;
+blockchain: string;
+name: string;  
+description: string;  
+ownerName?: string;
+bio?: string;
+artwork?: string;
+website?: string;
+socials?: string;
+favorites?: string;
+code: string;
+followings?: string;
+publishableKeys?:string;
+mimeType: string;   
+version: string;   
  
 }
